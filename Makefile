0b4b4c62e11c715d06ab243492868e0d5a15608a
UnderScore <drojasg25@gmail.com>
2024-09-23 01:58:15 -0400

    Clickers

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
+CXXFLAGS=-Wall - pipe -std=c++17
+
 all:
-	g++ -I src/include -L src/lib -o program program.cpp -lmingw32 -lSDL2main -lSDL2
+	g++ -I src/include -L src/lib -o program program.cpp -lmingw32 -lSDL2main -lSDL2 -lopengl32 -lglfw3 -lglm 
--- program.cpp
+++ program.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <stdio.h>
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_audio.h>
+
+#include <vulkan/vulkan.h>
 
 const int WIDTH = 800, HEIGHT = 600;
 
@@ -9,7 +12,7 @@ int main(int argc, char *argv[]){
     // Initilization 
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window *window = SDL_CreateWindow("UwU SDL2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
+    SDL_Window *window = SDL_CreateWindow("UwU SDL2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN);
 
     if (NULL == window){
         std::cout << "Could not create a window: " << SDL_GetError() << std::endl;
@@ -21,7 +24,11 @@ int main(int argc, char *argv[]){
 
     //Image Experiments
     SDL_Surface* image = SDL_LoadBMP("files/imgs/ipsumlorem.bmp");
+    SDL_Surface* image2 = SDL_LoadBMP("files/imgs/tedium.bmp");
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, image);
+    SDL_Texture* texture2 = SDL_CreateTextureFromSurface(renderer, image2);
+
+    
 
     //Audio Experiments
     SDL_AudioStream *stream = SDL_NewAudioStream(AUDIO_S16, 1, 22050, AUDIO_F32, 2, 48000);
@@ -38,10 +45,16 @@ int main(int argc, char *argv[]){
             if (SDL_QUIT == windowEvent.type){
                 break;
             }
+            if (SDL_MOUSEBUTTONDOWN == windowEvent.type){
+                SDL_RenderCopy(renderer, texture2, NULL, NULL);
+                SDL_RenderPresent(renderer);   
+            } else {
+                SDL_RenderCopy(renderer, texture, NULL, NULL);
+                SDL_RenderPresent(renderer);   
+            }
         }
+        
 
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
-        SDL_RenderPresent(renderer);   
     }
     
     SDL_DestroyWindow(window);
